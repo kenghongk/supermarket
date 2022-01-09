4d7638e0c067e7174bc9a62565080eb43c9fa0ff
fatin-hana <76954628+fatin-hana@users.noreply.github.com>
2022-01-10 04:02:04 +0800

    cartreal

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,27 +2,16 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< Updated upstream
-use Illuminate\Http\Request;
-use App\Models\Product;
-=======
 use App\Models\Product;
 use Illuminate\Http\Request;
->>>>>>> Stashed changes
 
 class ProductController extends Controller
 {
     public function productList()
     {
-<<<<<<< Updated upstream
-        $data = Product::all();
-
-        return view('Catalog.catalog', compact('data'));
-=======
         $products = Product::all();
 
         return view('products', compact('products'));
->>>>>>> Stashed changes
     }
 
     public function cartList()
@@ -34,33 +23,18 @@ class ProductController extends Controller
     {
         \Cart::add([
             'id' => $request->id,
-<<<<<<< Updated upstream
             'name' => $request->product_name,
             'price' => $request->price,
             'quantity' => $request->quantity,
             'attributes' => array(
-            	 'shop_id' => $request->shop_id,
                 'image' => $request->product_img,
-               
-                
-=======
-            'name' => $request->name,
-            'price' => $request->price,
-            'quantity' => $request->quantity,
-            'attributes' => array(
-                'image' => $request->image,
-                'vendor' => $request->vendor,
+                'vendor' => $request->shop_id,
 
 
->>>>>>> Stashed changes
             )
         ]);
         session()->flash('success', 'Product is Added to Cart Successfully !');
 
         return redirect()->route('cart.list');
     }
-<<<<<<< Updated upstream
 }
-=======
-}
->>>>>>> Stashed changes
