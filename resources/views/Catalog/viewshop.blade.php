ea88bcf6bbb9f4f9d4fea224965f4a7d0f77098c
fatin-hana <76954628+fatin-hana@users.noreply.github.com>
2022-01-10 18:02:23 +0800

    cart

--- resources/views/Catalog/viewshop.blade.php
+++ resources/views/Catalog/viewshop.blade.php
@@ -37,12 +37,17 @@
             <div class="down-content">
               <a href="#"><h4 style="text-align: left">{{$product->product_name}}</h4></a>
               <h6>RM {{$product->price}}</h6>
-              <form action="" method="post" style="text-align: left">
-                @csrf
-                <input type="number" value="1" min="1" class="form-control" style="width:100px" name="">
-                <br>
-              <input align="left" style="background-color: #4CAF50;" class="btn btn-success" type="submit" value="Add Cart">
-              </form>
+              <form action="{{ route('cart.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <input type="hidden" value="{{ $product->id }}" name="id">
+                        <input type="hidden" value="{{ $product->product_name }}" name="product_name">
+                        <input type="hidden" value="{{ $product->shop_id }}" name="shop_id">
+                         <input type="hidden" value="{{ $product->description }}" name="description">
+                        <input type="hidden" value="{{ $product->price }}" name="price">
+                        <input type="hidden" value="{{ $product->product_img }}"  name="product_img">
+                        <input type="hidden" value="1" name="quantity">
+                        <button class="btn btn-success">Add To Cart</button>
+                    </form>
             </div>
           </div>
         </div>
